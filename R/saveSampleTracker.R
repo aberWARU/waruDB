6e05c390cbd523be5cb930c0b8722624ee361110
wilsontom <wilsontom89@gmail.com>
2019-08-12 16:33:26 +0100

    save sample tracker data to the database

--- R/saveSampleTracker.R
+++ R/saveSampleTracker.R
@@ -4,27 +4,15 @@
 #'
 #' @param db_connection a `dbPool` or `RMariaDB` database connection
 #' @param data the reactive output from `formData`
-#' @return a numeric value of either 0 (failure) or 1 (success)
 #' @export
 
-saveSampleTracker <- function(db_connection, data) {
-  sample_tracker_tbl <-
-    db_connection %>% tbl('SampleTracker') %>% collect()
+saveSampleTracker <- function(formData, db_connection) {
+  query <- sprintf(
+    "INSERT INTO %s (%s) VALUES ('%s')",
+    'SampleTracker',
+    paste(names(data), collapse = ", "),
+    paste(data, collapse = "', '")
+  )
 
-  if (data[[1, 'hash']] %in% sample_tracker_tbl$hash) {
-    return(0)
-
-  } else{
-    query <- sprintf(
-      "INSERT INTO %s (%s) VALUES ('%s')",
-      'SampleTracker',
-      paste(names(data), collapse = ", "),
-      paste(data, collapse = "', '")
-    )
-
-    dbExecute(db_connection, query)
-
-    return(1)
-  }
-
+  dbExecute(db_connection, query)
 }
