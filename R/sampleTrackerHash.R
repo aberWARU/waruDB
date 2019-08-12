d992cfbedd55916a521de3fcbeb101d255fa2160
wilsontom <wilsontom89@gmail.com>
2019-08-12 16:32:09 +0100

    create hash code for incoming samples

---
+++ R/sampleTrackerHash.R
@@ -0,0 +1,22 @@
+#' Sample Tracker Hash Code
+#'
+#' A simple generation of a hash code which is used to check if an incoming sample already has an existing entry in the database.
+#'
+#' The hash code is simply the base64 encoded concatantion of; `Sample ID`, `Date Recieved` and `Sample Type`
+#'
+#' @param formData a character of the reactive output from the Sample Tracker form entry
+#' @return a character of the base64 encoded sample hash code
+#'
+#' @export
+
+sampleTrackerHash <- function(formData)
+  {
+
+  sampleHash <- paste0(formData[['sample_id']],
+                       formData[['date_recieved']],
+                       formData[['sample_type']])
+
+  sampleHashEnc <- openssl::base64_encode(charToRaw(sampleHash))
+
+  return(sampleHashEnc)
+  }
