e30c5b04ad14409768b7d8057d1cb88e9b75e970
wilsontom <wilsontom89@gmail.com>
2019-08-12 16:30:03 +0100

    check incoming sample hash in the database

---
+++ R/checkSampleTrackerHash.R
@@ -0,0 +1,24 @@
+#' Check Sample Tracker Hash
+#'
+#' Check if the incoming Sample Tracker entry already exists in the database
+#'
+#' @param formData a character of the reactive output from the Sample Tracker form entry
+#' @param db_connection a `dbPool` or `RMariaDB` database connection
+#' @return a numeric value either 0 (hash already exists) 1 (incoming hash is new)
+#'
+#' @export
+
+checkSampleTrackerHash <- function(formData, db_connection)
+{
+  incomingHash <- sampleTrackerHash(formData)
+
+  sampleTrackerHashVec <-
+    db_connection %>% tbl('SampleTracker') %>% select(hash) %>% pull()
+
+  if (incomingHash %in% sampleTrackerHashVec) {
+    return(0)
+  } else{
+    return(1)
+  }
+
+}
