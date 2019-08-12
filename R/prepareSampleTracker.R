47572a7bc7ecfc83eaefefb96fdfe782cfc54a98
wilsontom <wilsontom89@gmail.com>
2019-08-12 16:31:00 +0100

    prepare sample tracker form data ready for submission

---
+++ R/prepareSampleTracker.R
@@ -0,0 +1,32 @@
+#' Prepare Sample Tracker Form
+#'
+#' Prepare incoming Sample Tracker form data for input into the database
+#'
+#' @param formData a character of the reactive output from the Sample Tracker form entry
+#' @param username a character of the current session user (ie, `USER$name`)
+#' @return a character for prepared `formData` or a list of urine formData and DBS formData
+#'
+#' @export
+
+prepareSampleTracker <- function(formData, username)
+{
+  formData <- c(formData,
+                timestamp = as.character(Sys.time()),
+                userstamp = username)
+
+  formData[['date_recieved']] <-
+    as.Date(as.numeric(formData[['date_recieved']]), origin = '1970-01-01')
+
+  formData[['tubes']] <- as.numeric(formData[['tubes']])
+
+
+  if (formData[['dbs_card']] == 'TRUE') {
+    DBS_Input <- DBSform(formData)
+
+    return(list(formData, DBS_Input))
+
+  } else{
+    return(formData)
+  }
+
+}
