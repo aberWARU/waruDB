771779a9f4ac2a2b45d4b2e9b07fc9d1f8f434f4
wilsontom <wilsontom89@gmail.com>
2019-08-20 16:53:29 +0100

    prepare contact form data

---
+++ R/prepareContactForm.R
@@ -0,0 +1,26 @@
+#' Prepare Contact Infomation Form
+#'
+#' Prepare incoming Contact Form data for input into the database
+#'
+#' @param formData a character of the reactive output from the Contact Form entry
+#' @param username a character of the current session user (ie, `USER$name`)
+#' @return a list of prepared form data
+#'
+#' @export
+
+prepareContactForm <- function(formData, username)
+  {
+
+  formData <- as.list(formData)
+
+  formData$timestamp = as.character(Sys.time())
+  formData$userstamp = username
+
+
+  formData$DOB <-
+    as.character(as.Date(as.numeric(formData$DOB), origin = '1970-01-01'))
+
+
+  return(formData)
+
+  }
